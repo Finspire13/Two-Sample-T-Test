20d38cef6cdf1f91e480a2f8f293f37ccfb86b4a
Daochang Liu <finspire13@gmail.com>
2024-04-25 01:08:26 +1000

    Update app.py

--- app.py
+++ app.py
@@ -7,39 +7,35 @@ from sqlalchemy.sql import func
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vseryxnppudcar:9b8024182a9e9b69cf2cf7ea5da8446142c4a83219cf6f9d358e50742f9af8fc@ec2-44-206-204-65.compute-1.amazonaws.com:5432/da5jm4vashv4et'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
-
 class Sample(db.Model):
-    sample_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.String(100), nullable=False)
     gender = db.Column(db.String(100), nullable=False)
     def __repr__(self):
-        return f'<Sample {self.sample_id}>'
-
-
-
-
-numbers = []
-genders = []
+        return f'<Sample {self.id}>'
 
 @app.route('/', methods=["GET","POST"])
 def index():
 
     if request.method == "POST":
-        number = str(request.form['wkp-time'])
+        time = str(request.form['wkp-time'])
         gender = str(request.form['gender'])
-        numbers.append(number)
-        genders.append(gender)
+        sample = Sample(time=time, gender=gender)
+        db.session.add(sample)
+        db.session.commit()
 
-    output = ''
-    for i in range(len(numbers)):
-        output += f'{numbers[i]}, {genders[i]}\n'
+    all_samples = Sample.query.all()
+    output = str(all_samples)
+    print(all_samples)
+
+    # output = ''
+    # for i in range(len(numbers)):
+    #     output += f'{numbers[i]}, {genders[i]}\n'
 
     return render_template('index.html', output=output)
 
@@ -56,11 +52,11 @@ def index():
 #         return_text += f'{numbers[i]}, {genders[i]}\n'
 #     return return_text
 
-@app.route('/reset')
-def reset():
-    numbers = []
-    genders = []
-    return 'Reset Successfully!'
+# @app.route('/reset')
+# def reset():
+#     numbers = []
+#     genders = []
+#     return 'Reset Successfully!'
 
 if __name__ == '__main__':
     app.run(debug=True)
